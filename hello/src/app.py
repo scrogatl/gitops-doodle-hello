27d14daa7cf641c5d64bf5f96b1b326d11fd6e9f
scrogatl <scott@scottandrewrogers.com>
2024-10-17 08:54:27 +0000

    cleaned up dockerfile and added errors to hello

--- hello/src/app.py
+++ hello/src/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from time import sleep
 import os
+from random import randrange
 
 app = Flask(__name__)
 
@@ -10,7 +11,11 @@ def hello_world():
     shard = os.environ.get('SHARD', "na")
     
     print("SHARD: " + shard)
-    return "Hello (" + shard + ")" 
+    r = randrange(100)
+    if r < 26:
+        return "Error", 500
+    else: 
+        return "Hello (" + shard + ")" 
 
 @app.route("/hash")
 def get_hash():
