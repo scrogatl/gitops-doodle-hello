fc5c93d5ccd473c67550806539e3180460b21efb
scrogatl <scott@scottandrewrogers.com>
2024-11-14 14:54:37 +0000

    added env for error rate

--- hello/src/app.py
+++ hello/src/app.py
@@ -6,13 +6,21 @@ from random import randrange
 
 app = Flask(__name__)
 
+def logit(message):
+    timeString = datetime.now().strftime("%H:%M:%S.%f")[:-3]
+    log.info(timeString + " - [frontend: " + shard + "] - " + message)
+
+shard = os.environ.get('SHARD', "na")
+errorThresh = os.environ.get('ERROR_THRESH', "50")
+
+logit("errorThresh: " + errorThresh)
+
 @app.route("/")
 def hello_world():
-    shard = os.environ.get('SHARD', "na")
     
     print("SHARD: " + shard)
     r = randrange(100)
-    if r < 60:
+    if r < int(errorThresh):
         abort(500)
     else: 
         return "Hello (" + shard + ")" 
