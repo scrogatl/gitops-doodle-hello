d1b031578a6cbeca1a68289ca19090940c24aad8
scrogatl <scott@scottandrewrogers.com>
2024-11-19 17:18:20 +0000

    added call to weather service

--- hello/src/app.py
+++ hello/src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, abort
+import requests
 from flask import request
 from time import sleep
 import os
@@ -13,6 +14,8 @@ app = Flask(__name__)
 
 shard       = os.environ.get('SHARD', "na")
 errorThresh = os.environ.get('ERROR_THRESH', "50")
+weatherHost = os.environ.get('WEATHER_HOST', "weather")
+weatherPort = os.environ.get('WEATHER_PORT', "5100")
 
 def logit(message):
     timeString = datetime.now().strftime("%H:%M:%S.%f")[:-3]
@@ -25,7 +28,23 @@ def hello():
     if r < int(errorThresh):
         abort(500)
     else: 
-        return "Hello (" + shard + ")" 
+        rr = randrange(100)
+        if rr < int(errorThresh):
+            try:
+                resWeather = requests.get('http://weather:5100/weatherforecast')
+                print(resWeather)
+                logit("resWeather.text: " + resWeather.text)
+                logit("resWeather.status_code: " + str(resWeather.status_code))
+                return resWeather.text
+            except Exception as e:
+                logit(str(e))
+                print("type of exception:")
+                print(type(e))
+                print(e.errno)
+                print("e", e)
+                return str(e)
+        else:
+            return "Hello (" + shard + ")" 
 
 @app.route("/hash")
 def get_hash():
