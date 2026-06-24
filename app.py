62919c21a368355b5c57b6415b3d3596e1cd8b33
scrogatl <scott@scottandrewrogers.com>
2026-06-24 18:05:38 -0400

    updated app.py

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import Flask, abort, request
+from datetime import datetime
+import logging
+
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.DEBUG)
+
 app = Flask(__name__)
+
+def logit(message):
+    timeString = datetime.now().strftime("%H:%M:%S.%f")[:-3]
+    log.debug(timeString + " - [hello:] - " + message)
+
 
 @app.route("/")
 def hello():
-    return "<html><body><h1>Hello Best Bike App!</h1></body></html>\n"
+    logit("---- HEADERS BEGIN -----")
+    for header, value in request.headers.items():
+        logit(f"{header}: {value}")
+    logit("---- HEADERS END -----")
+    return "<html><body><h1>Hello!</h1></body></html>"
